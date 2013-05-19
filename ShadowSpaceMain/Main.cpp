2d59de760e5cde6eeb5a5e7aaed97c46451b73a2
Brett <beersbr@gmail.com>
2013-05-19 01:02:35 -0600

    + updated the code to include the allegro5 ttf addon and a font file

--- ShadowSpaceMain/Main.cpp
+++ ShadowSpaceMain/Main.cpp
@@ -1,5 +1,8 @@
 #include <allegro5\allegro5.h>
 #include <allegro5\allegro_primitives.h>
+#include <allegro5\allegro_direct3d.h>
+#include <allegro5\allegro_font.h>
+#include <allegro5\allegro_ttf.h>
 #include "HighResTimer.h"
 #include "InputHandler.h"
 #include "Particle.h"
@@ -20,6 +23,13 @@ int main(int argc, char** argv)
 	ALLEGRO_DISPLAY *display = al_create_display(1280, 800);
 	if(!display) return false;
 
+	al_init_font_addon();
+	al_init_ttf_addon();
+
+	ALLEGRO_FONT *font = al_load_font("fonts\\pirulen.ttf", 12, 0);
+
+	if(!font) return false;
+
 	int gamestate = PLAYING;
 	ALLEGRO_TIMER *timer = al_create_timer(1.0f/60.0f);
 	al_start_timer(timer);
@@ -31,44 +41,59 @@ int main(int argc, char** argv)
 	InputHandler* input = InputHandler::Instance();
 
 	Particle *p = new Particle();
-	p->accel.x = 1;
-	p->position.x = 0;
-	p->position.y = 400;
+	p->accel.x = 1.0f;
+	p->position.x = 0.0f;
+	p->position.y = 400.0f;
 
 	double start_time = hrtimer.getMilliseconds();
-	double elapsed_time = 0.0f;
-	double last_time = 0.0f;
+	double current_time = hrtimer.getMilliseconds();
+	const double dt = 1.0f/60.0f;
+	double t = 0.0f;
 
 	while(gamestate == PLAYING)
 	{
-		last_time = elapsed_time;
-		elapsed_time = hrtimer.getMilliseconds();
+		double new_time = hrtimer.getMilliseconds();
+		double frame_time = new_time - current_time;
+		current_time = new_time;
 
-		ALLEGRO_EVENT evt;
-		while(al_get_next_event(event_queue, &evt))
+		while(frame_time > 0.0f)
 		{
-			switch(evt.type)
+			ALLEGRO_EVENT evt;
+			while(al_get_next_event(event_queue, &evt))
 			{
-			case ALLEGRO_EVENT_KEY_DOWN:
-				input->setKey(evt.keyboard.keycode);
-				break;
-			case ALLEGRO_EVENT_KEY_UP:
-				input->unsetKey(evt.keyboard.keycode);
-				break;
-			case ALLEGRO_EVENT_DISPLAY_CLOSE:
-				gamestate = QUITTING;
-				break;
-			case ALLEGRO_EVENT_TIMER:
-				break;
+				switch(evt.type)
+				{
+				case ALLEGRO_EVENT_KEY_DOWN:
+					input->setKey(evt.keyboard.keycode);
+					break;
+				case ALLEGRO_EVENT_KEY_UP:
+					input->unsetKey(evt.keyboard.keycode);
+					break;
+				case ALLEGRO_EVENT_DISPLAY_CLOSE:
+					gamestate = QUITTING;
+					break;
+				case ALLEGRO_EVENT_TIMER:
+					break;
+				}
 			}
+
+			const float delta_time = min(frame_time, dt);
+
+
+			//p->update();
+
+			frame_time -= delta_time;
+			t += delta_time;
+
+
 		}
 
 		al_clear_to_color(al_map_rgb(0, 0, 0));
 
-		//p->update(0.016);
-		
-		al_flip_display();
+		al_draw_text(font, al_map_rgb(255, 255, 255), 350, 10, ALLEGRO_ALIGN_LEFT, "Allegro 5 Rocks!");
 
+		al_draw_filled_circle(100, 100, 100, al_map_rgb(255, 255, 255));
+		al_flip_display();
 	}
 
 	al_destroy_display(display);
